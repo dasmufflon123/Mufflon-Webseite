11fe73418630bbc10245e0e45760dd466b7793f4
dasmufflon123 <mufflonmighty@gmail.com>
2026-07-09 09:33:06 +0200

    Update news-data.js

--- Mufflonseite/js/news-data.js
+++ Mufflonseite/js/news-data.js
@@ -13,7 +13,7 @@ const NEWS_EINTRAEGE = [
   },  
   {
     datum: "2026-07-09",
-    titel: "Die Mufflon-Website ist online! 🐑",
+    titel: "Die Mufflon-Website ist online! test123123 🐑",
     text: "Willkommen auf der neuen Homepage! Hier findest du ab sofort News, das Archiv der Pflanze und bald auch das Freundesbuch.",
     tag: "Website"
   },  
